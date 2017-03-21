59c61365e1e4ac501e876158c93c11037dd380fd
qweeze <g.fortunatov@bars-open.ru>
2017-03-21 21:22:17 +0300

    some more tests

---
+++ tests/test_calc.py
@@ -0,0 +1,25 @@
+import pytest
+from python_parser.examples.calc import parser, interpreter, InterpreterError
+
+
+def test_calc():
+
+    interpreter.visit(parser.parse('PROGRAM', 'a = 11'))
+    interpreter.visit(parser.parse('PROGRAM', 'b = -13'))
+    a, b = 11, -13
+
+    samples = (
+        '2+2',
+        '(((23 + -3.2)) / 34+(2*((3)) *(3.0 - .1)))',
+        '0.000',
+        'a * b - 123'
+    )
+
+    for i in samples:
+        assert interpreter.visit(parser.parse('PROGRAM', i)) == eval(i)
+
+    with pytest.raises(ZeroDivisionError):
+        interpreter.visit(parser.parse('PROGRAM', '1 / 0'))
+
+    with pytest.raises(InterpreterError):
+        interpreter.visit(parser.parse('PROGRAM', '1 + undefined'))
---
+++ tests/test_json_loader.py
@@ -0,0 +1,23 @@
+import pytest
+import json
+from python_parser.examples.json_parser import load as json_loads
+
+
+def test_json_loader():
+    samples = (
+        {
+            'first': [1, 2, 3, {'5': 6}, True],
+            'key': 'value',
+            '1': 2,
+        },
+        [
+            {'test': 'test'},
+            None,
+            {'test': False},
+            -321.123
+        ],
+        {},
+        []
+    )
+    for sample in samples:
+        assert json_loads(json.dumps(sample)) == sample
